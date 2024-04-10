a672ee64f78563bfadf0f3d4b1e6fd4bdf15023a
Manuel Belotti <manuelbelotti821@gmail.com>
2024-04-10 18:15:18 +0200

    Aggiunta libreria per la gestione, visualizzazione ed editing spartiti (PSAMControlLibrary)

--- MusicalProject/Form1.cs
+++ MusicalProject/Form1.cs
@@ -3,18 +3,48 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PSAMControlLibrary;
 
 namespace MusicalProject
 {
     public partial class Form1 : Form
     {
+        IncipitViewer viewer;
+
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
+
+/*
+            viewer = new IncipitViewer();
+            viewer.Dock = DockStyle.Fill;
+
+            Clef c = new Clef(ClefType.GClef, 2);
+            viewer.AddMusicalSymbol(c);
+
+            Note n = new Note("G", 0, 4, MusicalSymbolDuration.Quarter, NoteStemDirection.Up, NoteTieType.None, new List<NoteBeamType>() { NoteBeamType.Single });
+            viewer.AddMusicalSymbol(n);
+
+            Note n1 = new Note("B", 0, 4, MusicalSymbolDuration.Sixteenth, NoteStemDirection.Up, NoteTieType.None, new List<NoteBeamType>() { NoteBeamType.Single });
+            viewer.AddMusicalSymbol(n1);
+
+            Rest r = new Rest(MusicalSymbolDuration.Quarter);
+            viewer.AddMusicalSymbol(r);
+
+            //aggiungi viewer a tabPage1
+            Controls.Add(viewer);
+*/
