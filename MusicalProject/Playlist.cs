1773caf4214d1b5bd576c7f175e7a79c7fd24244
Manuel Belotti <manuelbelotti821@gmail.com>
2024-04-10 19:47:04 +0200

    Impostazione (fino al costruttore) classe Playlist

--- MusicalProject/Playlist.cs
+++ MusicalProject/Playlist.cs
@@ -8,5 +8,40 @@ namespace MusicalProject
 {
     internal class Playlist
     {
+        //attributi
+        private string _titolo;
+        private string _descrizione;
+        private DateTime _datacreazione;
+        private List<Brano> _brani;
+
+        //proprerties su una riga
+        public string Titolo { get => _titolo; set => _titolo = value; }
+        public string Descrizione { get => _descrizione; set => _descrizione = value; }
+        public DateTime Datacreazione { get => _datacreazione; set => _datacreazione = value; }
+        public List<Brano> Brani { get => _brani; set => _brani = value; }
+         
+        //costruttori (dati, vuoto, copia)
+        public Playlist(string titolo, string descrizione, DateTime datacreazione, List<Brano> brani)
+        {
+            Titolo = titolo;
+            Descrizione = descrizione;
+            Datacreazione = datacreazione;
+            Brani = brani;
+        }
+        public Playlist()
+        {
+            Titolo = "";
+            Descrizione = "";
+            Datacreazione = DateTime.Now;
+            Brani = new List<Brano>();
+        }
+        public Playlist(Playlist p)
+        {
+            Titolo = p.Titolo;
+            Descrizione = p.Descrizione;
+            Datacreazione = p.Datacreazione;
+            Brani = p.Brani;
+        }
+
     }
 }
