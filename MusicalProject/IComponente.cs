4ee3842ffb1791e0188afe78c35208c6803645dc
Manuel Belotti <manuelbelotti821@gmail.com>
2024-04-10 19:27:55 +0200

    Creazione classe IComponente per pattern Composite, creazione dei vari file per ogni classe prevista

---
+++ MusicalProject/IComponente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicalProject
+{
+    internal interface IComponente
+    {
+        void Add(IComponente c);
+        void Remove(IComponente c);
+        void GetChild(int i);
+        string ToString();
+        bool Equals(object obj);
+    }
+}
